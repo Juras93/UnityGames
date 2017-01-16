32ba85e2825d864ac072d6bd11a6a01e0f6d96f3
Juras93 <jerzy.gawor@gmail.com>
2017-01-16 20:02:32 +0100

    Add missing files and script
    - commit missing files from previous  merge

---
+++ Tutorials/Text101/Assets/TextController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+	public Text text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			text.text = "Space clicked";
+		}
+	}
+}
